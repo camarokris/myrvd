cc7a3250c603bf7888d9cd121077b9ec99da5784
camarokris <camarokris@gmail.com>
2023-10-23 20:31:51 -0500

    okay

--- voiceover.py
+++ voiceover.py
@@ -1,10 +1,14 @@
-import pyttsx3
+#import pyttsx3
+from gtts import gTTS
 
 voiceoverDir = "Voiceovers"
 
 def create_voice_over(fileName, text):
     filePath = f"{voiceoverDir}/{fileName}.mp3"
-    engine = pyttsx3.init()
-    engine.save_to_file(text, filePath)
-    engine.runAndWait()
+    #engine = pyttsx3.init()
+    #engine.save_to_file(text, filePath)
+    #engine.runAndWait()
+    #del engine
+    tts = gTTS(text=text, lang='en')
+    tts.save(filePath)
     return filePath
